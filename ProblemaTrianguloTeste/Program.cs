2d4caa2038c9882e2d4bd31b854433a08e22169b
Rafael Bisol <rafaelpbisol@hotmail.com>
2024-02-23 00:31:52 -0300

    Project Created

---
+++ ProblemaTrianguloTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblemaTrianguloTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
